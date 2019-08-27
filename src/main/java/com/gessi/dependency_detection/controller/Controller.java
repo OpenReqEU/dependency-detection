6b9b597a56a35526b1be788ac216f140364e6bfe
Quim <joaquim.motger@est.fib.upc.edu>
2019-08-27 16:14:42 +0200

    fix bug in WIP

--- src/main/java/com/gessi/dependency_detection/controller/Controller.java
+++ src/main/java/com/gessi/dependency_detection/controller/Controller.java
@@ -15,11 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.gessi.dependency_detection.service.FileFormatException;
