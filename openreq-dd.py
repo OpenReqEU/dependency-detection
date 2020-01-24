086c289928129122b9f2d8849cf2c81b5d05d441
Quim <joaquim.motger@est.fib.upc.edu>
2020-01-24 14:05:29 +0100

    add post requests

--- openreq-dd.py
+++ openreq-dd.py
@@ -2,6 +2,10 @@ import requests
 import json
 import io
 
+URL = "http://localhost:9407/upc/dependency-detection/"
+WITHOUT_PERSISTENCE = "json/ontology/"
+WITH_PERSISTENCE = "persistence/analysis/"
+
 def initPrompt():
 	print("Please type a number to perform a specific action:")
 	print("")
@@ -47,7 +51,7 @@ def askSyn():
 		return -1
 	else:
 		print("!!! INVALID ANSWER")
-		askSyn()
+		return askSyn()
 
 def selectSynThreshold():
 	print("Select a threshold synonymy value [0..1]: ", end = ' ')
@@ -56,41 +60,45 @@ def selectSynThreshold():
 		return threshold
 	else:
 		print("!!! INVALID SYNONYMY THRESHOLD")
-		selectSynThreshold()
+		return selectSynThreshold()
 
 def selectReqFile():
-	print("Input the relative path to the file containing the requirements in JSON format (e.g., ./files/requirements.json): ", end = ' ')
+	print("Input the relative path to the file containing the requirements in JSON format (e.g., ./confidential_Apata-Cata_RBC.json): ", end = ' ')
 	reqPath = input()
 	try:
 		with io.open(reqPath, 'r', encoding='utf-8-sig') as json_file:
-			json_object = json.load(json_file)
-			return json_object
+			#json_object = json.load(json_file)
+			#return json_object
+			return reqPath
 	except EnvironmentError:
 		print("!!! WRONG REQUIREMENTS PATH FILE")
 		return selectReqFile()
 
 def selectOntologyFile():
-	print("Input the relative path to the ontology file (e.g., ./files/ontology.owl): ", end = ' ')
+	print("Input the relative path to the ontology file (e.g., ./openreq-rail-small.owl): ", end = ' ')
 	ontology = input()
 	try:
 		with io.open(ontology, 'r', encoding='utf-8-sig') as owl_file:
-			return owl_file
+			return ontology
 	except EnvironmentError:
 		print("!!! WRONG ONTOLOGY PATH FILE")
 		return selectOntologyFile()
 
 def selectProject(reqs):
-	print("Input the project ID (e.g., OPENREQ-PROJECT): ", end = ' ')
+	print("Input the project ID (e.g., Siemens): ", end = ' ')
 	project = input()
-	found = False
-	for p in reqs['projects']:
-		if p['id'] == project:
-			found = True
-	if (found):
-		return project
-	else:
-		print("!!! PROJECT DOES NOT EXISTS IN REQUIREMENTS DATASET")
-		selectProject(reqs)
+	with io.open(reqs, 'r', encoding='utf-8-sig') as json_file:
+			#json_object = json.load(json_file)
+		json_object = json.load(json_file)
+		found = False
+		for p in json_object['projects']:
+			if p['id'] == project:
+				found = True
+		if (found):
+			return project
+		else:
+			print("!!! PROJECT DOES NOT EXISTS IN REQUIREMENTS DATASET")
+			return selectProject(reqs)
 
 def selectAnalysisId():
 	print("Input the analysis ID (e.g., 1268948486424): ", end = ' ')
@@ -107,6 +115,15 @@ def selectReq2():
 	req = input()
 	return req
 
+def sendRequest(path_request, reqs, ontology):
+	with io.open(reqs, 'r', encoding='utf-8-sig') as req_file:
+		with io.open(ontology, 'r', encoding='utf-8-sig') as ont_file:
+			print("")
+			print("Sending request...")
+			res = requests.post(path_request, files=(("ontology", ont_file), ("json", req_file)))
+			print("Response ", res)
+			print("")
+
 def runAnalysisAndReturn():
 	reqs = selectReqFile()
 	ontology = selectOntologyFile()
@@ -114,12 +131,28 @@ def runAnalysisAndReturn():
 	tool = selectKeywordTool()
 	syn = askSyn()
 
+	path_request = URL + WITHOUT_PERSISTENCE + "/" + project + "?keywordTool=" + tool
+	if (syn == -1):
+		path_request += "&synonymy=false"
+	else:
+		path_request += "&synonymy=true&threshold=" + syn
+
+	sendRequest(path_request, reqs, ontology)
+
 def runAnalysis():
 	reqs = selectReqFile()
 	ontology = selectOntologyFile()
 	project = selectProject(reqs)
 	tool = selectKeywordTool()
 	syn = askSyn()
+
+	path_request = URL + WITH_PERSISTENCE + "?projectId=" + project + "&keywordTool=" + tool
+	if (syn == -1):
+		path_request += "&synonymy=false"
+	else:
+		path_request += "&synonymy=true&threshold=" + syn
+
+	sendRequest(path_request, reqs, ontology)
 
 def getDependenciesBetweenRequirements():
 	analysisId = selectAnalysisId()
